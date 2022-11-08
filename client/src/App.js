d464a25859b6c5364f175cf35576325b3162232c
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-08 15:03:16 -0800

    Convert ViewAccount and ViewOpenings to React

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ViewAccount from './components/ViewAccount/ViewAccount';
+import ViewOpenings from './components/ViewOpenings/ViewOpenings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <ViewAccount />
+      <ViewOpenings />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
