28ae7f3bd18387aee990bb7885f70968e8a007a4
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-08 22:32:15 -0800

    Setup basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from "react-router-dom"
 import ViewAccount from './components/ViewAccount/ViewAccount';
 import ViewOpenings from './components/ViewOpenings/ViewOpenings';
 import ViewSubmittedApps from './components/ViewSubmittedApps/ViewSubmittedApps';
 
 function App() {
   return (
-    <div className="App">
-      <ViewAccount />
-      <ViewOpenings />
-      <ViewSubmittedApps />
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/ViewOpenings">View Openings</Link></li>
+          <li><Link to="/ViewSubmittedApps">View Submitted Applications</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ViewAccount />} />
+        <Route path="/ViewOpenings" element={<ViewOpenings />} />
+        <Route path="/ViewSubmittedApps" element={<ViewSubmittedApps />} />
+      </Routes>
+    </>
   );
 }
 
