59fc6a7a1dd2e2a79fccb5c2150b2f9dc587cae8
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-11 11:33:43 -0800

    Refactor job posting card code

--- client/src/components/Business/JobPostingsListCompany/JobPostingsList.js
+++ client/src/components/Business/JobPostingsListCompany/JobPostingsList.js
@@ -2,6 +2,7 @@ import './JobPostingsList.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navigation from '../../Navigation/Navigation';
+import JobPostingCard from '../JobPostingCard/JobPostingCard';
 
 function JobPostingsList() {
   return (
@@ -11,132 +12,11 @@ function JobPostingsList() {
         <h1> All Job Postings</h1>
         <div class="container mt-5 mb-3">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card p-3 mb-2 border-success">
-                        <div class="d-flex justify-content-between">
-                            <div class="d-flex flex-row align-items-center">
-                                <div class="ms-2 c-details">
-                                <button type="submit" class="btn btn-success">Edit Job Posting</button>
-                                </div>
-                            </div>
-                            <div class="badge"> <span>Tags</span> </div>
-                        </div>
-                        <div class="mt-5">
-                            <h3 class="heading">Job Title</h3>
-                            <div class="mt-5">
-                                <div class="mt-3"> <span class="text1">15 Positions left</span> </div>
-                                <div class="mt-3"> <span class="text1">32 Applied <span class="text2">of 50 capacity</span></span> </div>
-                                <br></br>
-                                <button type="submit" class="btn btn-success">View Applications</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <JobPostingCard />
+                <JobPostingCard />
+                <JobPostingCard />
+                <JobPostingCard />
+                <JobPostingCard />
             </div>
         </div>
     </div>
