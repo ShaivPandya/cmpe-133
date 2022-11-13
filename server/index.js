e55e814c856cf5da07a6ebc98476121a2149cecf
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-13 11:24:09 -0800

    Add api methods

--- server/index.js
+++ server/index.js
@@ -17,6 +17,37 @@ app.get("/", (req, res) => {
   res.json("hello");
 });
 
+// TODO: check if valid sign in
+app.get("/signIn", (req, res) => {
+  const q = "SELECT * FROM Users WHERE email='johndoe@gmail.com' AND password='password123';";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    // figure out a way to like deny entry based on result
+    // prolly more of a frontend thing
+    return res.json(data);
+  })
+})
+
+// signup (works with Insomnia)
+app.post("/signUp", (req, res) => {
+  const q = "INSERT INTO Users(`email`, `name`, `password`) VALUES (?)";
+
+  const values = [
+    req.body.email,
+    req.body.name,
+    req.body.password
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// get all jobs
 app.get("/jobs", (req, res) => {
   const q = "SELECT * FROM Jobs";
   db.query(q, (err, data) => {
@@ -27,6 +58,13 @@ app.get("/jobs", (req, res) => {
     return res.json(data);
   });
 });
+
+
+
+// create job
+app.post("/createJob", (req, res) => {
+
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend.");
