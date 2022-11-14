777dba3065b7542abf9909b76b96e2cf0cb44950
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-13 20:33:07 -0800

    Undo authentication stuff

--- client/src/components/signin/SignIn.js
+++ client/src/components/signin/SignIn.js
@@ -1,8 +1,7 @@
 import "./styles.css";
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import useAuth from "../../hooks/useAuth";
 
 export default function SignIn() {
   const [user, setUser] = useState({
