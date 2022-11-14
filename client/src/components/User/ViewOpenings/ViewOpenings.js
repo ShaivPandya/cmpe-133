59ca701d812bf64d1816a143be3a95b3409e8c95
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-13 17:40:37 -0800

    Connect View Openings Page to Database

--- client/src/components/User/ViewOpenings/ViewOpenings.js
+++ client/src/components/User/ViewOpenings/ViewOpenings.js
@@ -1,15 +1,37 @@
 import "./styles.css"
 import React,{ useState,useEffect } from 'react'
-import Axios from 'axios'
+import axios from "axios";
+import { useBootstrapBreakpoints } from "react-bootstrap/esm/ThemeProvider";
 
 function ViewOpenings() {
-    const [ jobList, setJobList ] = useState([]);
+    const [jobList, setJobList] = useState([]);
+    
+    useEffect(() => {
+        const fetchAllJobs = async() => {
+            try {
+                const res = await axios.get("http://localhost:8800/jobs")
+                setJobList(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchAllJobs();
+    }, [])
+
+    console.log(jobList);
 
     return(
         <div>
             <h1>View Job Openings</h1>
-            <div>
-                <input></input>
+            <div className="jobs">
+                {jobList.map((job) => (
+                    <div key={job.idJobs} className="book">
+                        <h2>{job.jobTitle}</h2>
+                        <p>{job.location}</p>
+                        <p>{job.description}</p>
+                        <button>Submit</button>
+                    </div>
+                ))}
             </div>
         </div>
     )
