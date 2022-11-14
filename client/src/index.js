03857dd962f6297b70f4a8a2152b7548089f970d
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2022-11-13 20:33:07 -0800

    Undo authentication stuff

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
         <App />
-      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
